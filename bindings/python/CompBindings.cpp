12690ac6d827e1e56a3edf24c7720a0c9541f1bb
Dmitriy Mikhalevich <mikhalevich@ispras.ru>
2024-06-27 18:38:32 +0300

    Fix of getEffectiveWidth() for integer constant expressions

--- bindings/python/CompBindings.cpp
+++ bindings/python/CompBindings.cpp
@@ -76,6 +76,7 @@ void registerCompilation(py::module_& m) {
         .def_readwrite("topModules", &CompilationOptions::topModules)
         .def_readwrite("paramOverrides", &CompilationOptions::paramOverrides)
         .def_readwrite("defaultLiblist", &CompilationOptions::defaultLiblist);
+        .def_readwrite("evalEffectiveWidth", &CompilationOptions::evalEffectiveWidth)
 
     py::class_<Compilation> comp(m, "Compilation");
     comp.def(py::init<>())
